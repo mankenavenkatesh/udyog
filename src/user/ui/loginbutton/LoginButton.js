08da2c20cd5cb01fe608e604e8ebd005b3d41a54
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-07-13 23:36:47 +0530

    Login Button CSS

--- src/user/ui/loginbutton/LoginButton.js
+++ src/user/ui/loginbutton/LoginButton.js
@@ -5,9 +5,9 @@ import React from 'react'
 
 const LoginButton = ({ onLoginUserClick }) => {
   return(
-    <li className="pure-menu-item">
-      <a href="#" className="pure-menu-link" onClick={(event) => onLoginUserClick(event)}>Login with UPort</a>
-    </li>
+    <div>
+      <a onClick={(event) => onLoginUserClick(event)}>Login with UPort</a>
+    </div>
   )
 }
 
